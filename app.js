67632a6963714748c90e76af9c002507c9930a61
Hendra Permana <hynra@yahoo.com>
2016-11-29 13:07:01 +0700

    Add mysql

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 var PROTO_PATH = __dirname + '/protos/helloworld.proto';
-
+var mysql      = require('mysql');
 var grpc = require('grpc');
+var appconfig = require('./appconfig.json');
 var hello_proto = grpc.load(PROTO_PATH).helloworld;
+var server = new grpc.Server();
 var test = require('./modules/test');
 
 
 
 
-function main() {
-  var server = new grpc.Server();
+
+function connectToGrpc(){
   server.addProtoService(hello_proto.Greeter.service, {sayHello: test.sayHellox});
-  server.bind('localhost:50051', grpc.ServerCredentials.createInsecure());
+  server.bind(appconfig.grpc['server']+appconfig.grpc['port'], grpc.ServerCredentials.createInsecure());
   server.start();
+  console.log("Running gRPC Server on Port "+appconfig.grpc['port']);
+}
+
+function connectToMysql(){
+  var connection = mysql.createConnection({
+    host     : appconfig.mysql['hostname'],
+    user     : appconfig.mysql['user'],
+    password : appconfig.mysql['password'],
+    database : appconfig.mysql['database']
+  });
+
+  connection.connect(function (err) {
+    if(err){
+      console.log("MySql : Connection to  "+appconfig.mysql['hostname'] + " error : "+err);
+    }else {
+      console.log("MySql :  Connection to  "+appconfig.mysql['hostname'] + " success");
+    }
+  });
+}
+
+var main = function () {
+  connectToGrpc();
+  connectToMysql();
 }
 
 main();
+
+
+
--- modules/test.js
+++ modules/test.js
@@ -1,5 +1,5 @@
-
 exports.sayHellox = function (call, callback) {
     callback(null, {message: 'Hello ' + call.request.name});
     console.log(call);
 }
+
